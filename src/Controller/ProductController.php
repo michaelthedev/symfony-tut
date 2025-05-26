76f1175ed28e8addfef91d43b8d8607d34b25293
Michael A <39175160+michaelthedev@users.noreply.github.com>
2025-05-26 17:25:58 +0100

    update products route url

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -6,9 +6,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+#[Route('/products')]
 final class ProductController extends AbstractController
 {
-    #[Route('/product', name: 'app_product')]
+    #[Route('/', name: 'products.index')]
     public function index(): Response
     {
         return $this->render('product/index.html.twig', [
