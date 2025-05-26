df70cff7f7ed0816f7fcb5e76ff221ed473c98c7
Michael A <39175160+michaelthedev@users.noreply.github.com>
2025-05-26 18:51:26 +0100

    wip: products crud

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -10,10 +13,36 @@ use Symfony\Component\Routing\Attribute\Route;
 final class ProductController extends AbstractController
 {
     #[Route('/', name: 'products.index')]
-    public function index(): Response
+    public function index(ProductRepository $repository): Response
     {
         return $this->render('product/index.html.twig', [
-            'controller_name' => 'ProductController',
+            'products' => $repository->findAll(),
         ]);
+    }
+
+    #[Route('/{id<\d+>}', name: 'products.show')]
+    public function show(Product $product): Response
+    {
+        return $this->render('product/show.html.twig', [
+            'product' => $product,
+        ]);
+    }
+
+    #[Route('/create', name: 'products.create')]
+    public function create(): Response
+    {
+        return $this->render('product/create.html.twig');
+    }
+
+    public function store(Request $request, ProductRepository $repository): Response
+    {
+        $product = new Product();
+        $product->setName($request->request->get('name'));
+        $product->setDescription($request->request->get('description'));
+        $product->setPrice((float) $request->request->get('price'));
+
+        // $repository->findBy($product);
+
+        return $this->redirectToRoute('products.index');
     }
 }
